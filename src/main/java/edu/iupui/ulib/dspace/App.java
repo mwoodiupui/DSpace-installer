f44334346857647802e25f72999e5e138b20875b
Mark H. Wood <mhwood@Ameritech.Net>
2013-12-26 11:17:13 -0500

    Eliminate copy-the-buildfile hack

--- src/main/java/edu/iupui/ulib/dspace/App.java
+++ src/main/java/edu/iupui/ulib/dspace/App.java
@@ -6,24 +6,30 @@
 package edu.iupui.ulib.dspace;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.apache.tools.ant.Project;
-import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.helper.ProjectHelper2;
 
 /**
  * Build an Ant project from an embedded build file.
  *
  * @author Mark H. Wood
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
-	throws Exception // TODO something nicer
+            throws URISyntaxException
     {
 	// Initialize an Ant project from the build file
 	Project project = new Project();
-	ProjectHelper.configureProject(project, getBuildFile());
+	//ProjectHelper.configureProject(project, getBuildFile());
+        ProjectHelper2 projectHelper = new ProjectHelper2();
+        URL buildResource = App.class.getResource("/build.xml");
+        URI buildURI = buildResource.toURI(); // XXX throws URISyntaxException
+        File buildFile = new File(buildURI);
+        projectHelper.parse(project, buildFile);
 
 	// Configure the project
 	project.setProperty("ivySettings",
@@ -34,34 +40,5 @@ public class App
 
 	// Install
 	project.executeTarget(target);
-    }
-
-    /**
-     * Horrible hack because Ant can't cope with non-File buildfiles.
-     * Copy the build.xml from an internal JAR resource to a temporary
-     * file.
-     *
-     * @return path to the scratch copy.
-     */
-    static private File getBuildfile()
-	throws Exception // TODO something nicer
-    {
-	InputStream configStream = App.class.getResourceAsStream("/build.xml");
-
-	File configFile = File.createTempFile("build", "xml");
-	configFile.deleteOnExit();
-	FileOutputStream tempStream = new FileOutputStream(configFile);
-
-	while (true)
-	    {
-		int next = configStream.read();
-		if (next < 0) break;
-		tempStream.write(next);
-	    }
-
-	configStream.close();
-	tempStream.close();
-
-	return configFile;
     }
 }
